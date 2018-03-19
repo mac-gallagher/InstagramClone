8dae6a58a4d7ba3565f3895e7b4d8a97fd3c0abe
mac-gallagher <jmgallagher36@gmail.com>
2018-03-18 23:53:16 -0500

    Add user profile picture functionality

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
     
@@ -60,6 +61,28 @@ class ViewController: UIViewController {
         return button
     }()
     
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     @objc func handleTextInputchange() {
         let isFormValid = emailTextField.text?.count ?? 0 > 0
             && usernameTextField.text?.count ?? 0 > 0
@@ -85,7 +108,35 @@ class ViewController: UIViewController {
                 return
             }
             
-            print("Sucessfully created user:", user?.uid ?? "")
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            let filename = NSUUID().uuidString
+            
+            Storage.storage().reference().child("profile_images").child(filename).putData(uploadData, metadata: nil, completion: { (metadata, err) in
+                if let error = err {
+                    print("Failed to upload profile image:", error)
+                    return
+                }
+                
+                let profileImageUrl = metadata?.downloadURL()?.absoluteString
+                
+                guard let uid = user?.uid else { return }
+                
+                let dictionaryValues = ["username": username, "profileImageUrl": profileImageUrl]
+                let values = [uid: dictionaryValues]
+                
+                Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                    if let error = err {
+                        print("Failed to save user info into database:", error)
+                        return
+                    }
+                })
+
+                
+            })
+            
+            
         }
     }
     
